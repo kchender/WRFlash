9e88575e9a65a6989c935686db9b18e2293144db
Katrina Henderson <henderson.katrina33@gmail.com>
2022-08-17 16:48:50 -0700

    integrating API

--- server/server.js
+++ server/server.js
@@ -3,5 +3,8 @@ const express = require('express');
 const router = require('./router');
 const app = express();
 const port = process.env.PORT || 3000;
+const wr = require('wordreference-api');
+
+
 
 module.exports = app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/client/routes/NewDeck.jsx
+++ src/client/routes/NewDeck.jsx
@@ -5,6 +5,10 @@ const NewDeck = () => {
 
     //useState hook to update name of deck
     const [deckName, setDeckName] = useState('');
+    //usestate hook to gewt input word
+    const [word, setWord] = useState('');
+    //function to use API to get definition of word that was input
+
 
     return (
       <div key='new-deck' id='new-deck' className='new-deck'>
@@ -13,7 +17,13 @@ const NewDeck = () => {
 
         <div key='card-content' className='card-content'>
           <form key='card-input' className='card-input'>
-            <input type='text' name='word' className='word' placeholder='Word' />
+            <input 
+            type='text' 
+            name='word' 
+            className='word' 
+            placeholder='Word' 
+            onChange={(e) => setWord(word += e.target.value)}
+            />
             <select>
                 <option value='en'>English</option>
                 <option value='es'>Spanish</option>
@@ -21,7 +31,12 @@ const NewDeck = () => {
                 <option value='fr'>French</option>
             </select>
             {/* onClick= use API to get definition*/}
-            <input type='button' className='get-wr-def' value='Get WR def' />
+            <input 
+              type='button' 
+              className='get-wr-def' 
+              value='Get WR def' 
+            //   onClick={() => }
+            />
             <input type='text' name='definition' className='definition' placeholder='Definition' />
             <input type='submit' className='add-card' value='Add To Deck' />
           </form>
